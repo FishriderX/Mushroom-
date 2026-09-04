cf5416377619eb7ab539d4abc41ddeebe22016e6
FishriderX <57499549+FishriderX@users.noreply.github.com>
2026-09-04 10:13:18 +0800

    Bump Mushroom Helper to V0.4.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.example.pikminhelper"
         minSdk = 30
         targetSdk = 35
-        versionCode = 12
-        versionName = "0.4.0"
+        versionCode = 13
+        versionName = "0.4.1"
     }
 
     compileOptions {
